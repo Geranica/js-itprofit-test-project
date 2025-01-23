3607a2bab03042b0f0424277f9d17a69c73887c3
Geranica <108093849+Geranica@users.noreply.github.com>
2025-01-23 10:03:36 +0300

    feat: Implement phone mask module

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,14 @@
+'use strict';
+
 import './../index.html';
 import './../index.scss';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+import { phoneMask } from './modules/phoneMask/phoneMask';
+import { MASK_TEMPLATES } from './modules/phoneMask/constants';
+
+window.addEventListener('DOMContentLoaded', () => {
+  phoneMask('#phone-input', MASK_TEMPLATES.DE);
+});
